d6ae24f5229ca2acaef74fe0037c4c1051f75aea
ElVi2 <48282124+ElVi2@users.noreply.github.com>
2020-06-02 16:42:35 +0200

    Fixed the warnings

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,26 @@
 extern crate serde_derive;
 
 //use std::io;
+use std::io::Read;
+use std::fs::File;
+use serde::{Serialize, Deserialize};
+use serde_xml;
+//use std::mem;
+//use std::io::BufReader;
 mod process_bpmn;
 
 fn main() {
-    let _test=process_bpmn::Process::ProcessId(3);
-    println!("Hello, world!");
+    let _test=process_bpmn::FlowObject::Activity;
+    println!("Please input the path:");
+    //let mut file_path = String::new();
+    //io::stdin()
+        //.read_line(&mut file_path)
+        //.expect("Failed to read line!");
+    let file_path="F:\\Models.Diagrams\\Call Activity.bpmn";
+    println!("{}", file_path);
+    let mut bpmn_file = File::open(file_path).unwrap();
+    let mut contents = String::new();
+    bpmn_file.read_to_string(&mut contents);
+    let deserialized: process_bpmn::Process = serde_xml::from_str(&contents).unwrap();
+    println!("{}", contents);
 }
--- src/process_bpmn.rs
+++ src/process_bpmn.rs
@@ -1,4 +1,43 @@
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Process {
-    ProcessId(i128),
+
+#[allow(dead_code)]
+pub enum Event {
+    StartEvent,
+    IntermediateEvent,
+    EndEvent
+}
+
+#[allow(dead_code)]
+pub enum Activity {
+    Task,
+    SubProcess,
+    Transaction,
+    CallActivity
+}
+
+#[allow(dead_code)]
+pub enum Gateway {
+    ExclusiveGateway,
+    EventBasedGateway,
+    ParallelGateway,
+    InclusiveGateway,
+    ExclusiveEventBasedGateway,
+    ParallelEventBasedGateway,
+    ComplexGateway
+}
+
+#[allow(dead_code)]
+pub enum FlowObject {
+    Event(Event),
+    Activity(Activity),
+    Gateway(Gateway)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Semantic;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Process {
+    id: u64,
+    semantic: Semantic
 }
