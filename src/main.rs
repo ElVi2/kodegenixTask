6c1dd133a1d8aa5fdb5ff7f134266d2e6990cce3
ElVi2 <48282124+ElVi2@users.noreply.github.com>
2020-06-02 12:46:42 +0200

    Started defining the enumerations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use serde::{Serialize, Deserialize};
+use std::io;
+mod process_bpmn;
 
 fn main() {
+    let mut test=process_bpmn::Process::ProcessId(3);
     println!("Hello, world!");
 }
---
+++ src/process_bpmn.rs
@@ -0,0 +1,3 @@
+pub enum Process {
+    ProcessId(usize),
+}
